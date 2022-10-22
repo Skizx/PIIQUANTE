60cd51778dacce456e97500ef02a45be9ec3303a
Kevin <kevin.le.milanais@hotmail.fr>
2022-10-22 05:55:25 +0200

    Création d'un shéma de données contenant les champs souhaités

---
+++ backend/models/Sauce.js
@@ -0,0 +1,20 @@
+// Importation de Mongoose
+const mongoose = require('mongoose');
+
+// Création d'un Schéma de données contenant les champs souhaités selon "Data ModelsSauce"
+const sauceSchema = mongoose.Schema({
+    userId: { type: String, required: true },
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true, min: 1, max: 10 },
+    likes: { type: Number, défault: 0, required: true },
+    dislikes: { type: Number, défault: 0, required: true },
+    usersLiked: { type: [String], required: true },
+    usersDisliked: { type: [String], required: true },
+});
+
+// Exportation du schéma
+module.exports = mongoose.model('Sauce', sauceSchema)
