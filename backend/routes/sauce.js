4cbc784b93cfd96758312a2aecfd832e0412f442
Kevin <kevin.le.milanais@hotmail.fr>
2022-10-23 11:59:35 +0200

    Importation du middleware d'authentification pour chaque routes

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,14 +1,17 @@
-// Importation d'Express, SauceSchema
+// Importation d'Express, SauceSchema, middleware auth
 const express = require('express');
 const sauceCtrl = require('../controllers/sauce');
+const auth = require('../middleware/auth');
+
 // Création du routeur Express
 const router = express.Router();
 
-router.post("/", sauceCtrl.createSauce)
-router.put('/:id', sauceCtrl.modifySauce)
-router.delete('/:id', sauceCtrl.deleteSauce)
-router.get('/:id', sauceCtrl.getOneSauce)
-router.get('/', sauceCtrl.getAllSauces)
+// Importation du middleware auth permettant de proteger, verifier les routes
+router.post("/", auth, sauceCtrl.createSauce)
+router.put('/:id', auth, sauceCtrl.modifySauce)
+router.delete('/:id', auth, sauceCtrl.deleteSauce)
+router.get('/:id', auth, sauceCtrl.getOneSauce)
+router.get('/', auth, sauceCtrl.getAllSauces)
 
 // Exportation du router
 module.exports = router;
