35d47da389152f74116dcd0531170106c0b77bd4
Kevin <kevin.le.milanais@hotmail.fr>
2022-10-25 18:18:03 +0200

    Modification route POST, ajout likes, dislikes etc ....

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -6,11 +6,13 @@ const fs = require('fs');
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce)
     delete sauceObject._id
-    delete sauceObject._userId
     const sauce = new Sauce ({
         ...sauceObject,
-        userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        likes: 0,
+        dislikes: 0,
+        usersLiked: [],
+        usersDisliked: []
     });
     sauce.save()
     .then(() => res.status(201).json({ message: 'Sauce enregistré !'}))
